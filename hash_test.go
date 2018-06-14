05d895ebe0915271539dbb5f114150e5ff69c7f5
Roman Müntener <me@mroman.ch>
2018-06-14 17:12:52 +0200

    + add new test cases

--- hash_test.go
+++ hash_test.go
@@ -1,5 +1,10 @@
 package libhash
 
+// There's not really a _need_ to be testing the hashes themselves
+// as we're expecting that each library has their own testing
+// capabilities... with the exception of libraries directly under
+// libhash/ which have their testing done here!
+
 import "testing"
 
 func checkHash(t *testing.T, hashf, hashs, hashe string) {
@@ -26,13 +31,19 @@ const L_AE = "abcde"
 const L_AH = "abcdefgh"
 const L_HW = "Hello, world!"
 
-func Test(t *testing.T) {
-	table := map[string]map[string]string{
-		"fletcher/16": map[string]string{
-			L_AE: "c8f0",
-			L_AH: "0627",
-		},
+func repeat(n int, a string) string {
+	b := ""
+	for i := 0; i < n; i++ {
+		b += a
+	}
+	
+	return b
+}
 
+func Test(t *testing.T) {
+
+	table := map[string]map[string]string{
+		// not in libhash/
 		"sha1": map[string]string{
 			L_STR: "650d340ea539598d225fdf54b2c9e850a04476cc",
 			L_HW:  "943a702d06f34599aee1f8da8ef9f7296031d699",
@@ -47,9 +58,40 @@ func Test(t *testing.T) {
 			L_AH:  "e8dc4081b13434b45189a720b77b6818",
 		},
 
+		// in libhash/
+		"fletcher/16": map[string]string{
+			L_AE: "c8f0",
+			L_AH: "0627",
+		},
+
 		"xor8": map[string]string{
 			L_AE: "61",
 			L_AH: "08",
+		},
+
+		"sum/8" : map[string]string {
+			L_HW : "89",
+			L_AE : "ef",
+			L_AH : "24",
+			repeat(4097, "A") : "41",
+		}, 
+
+		"sum/16" : map[string]string {
+			L_HW : "0489",
+			L_STR : "1307",
+			repeat(4097, "A") : "1041",
+		},
+
+		"sum/32" : map[string]string {
+			L_HW : "00000489",
+			L_STR : "00001307",
+			repeat(4097, "A") : "00041041",
+		},
+
+		"sum/64" : map[string]string {
+			L_HW : "0000000000000489",
+			L_STR : "0000000000001307",
+			repeat(4097, "A") : "0000000000041041",
 		},
 	}
 
